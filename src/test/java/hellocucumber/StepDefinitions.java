45f989eaf50ec2b0637cacedea066e25af568075
shad hasan <shasan@atlassian.com>
2021-01-27 17:39:06 +0530

    some change

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -5,10 +5,12 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.it.Ma;
 import io.restassured.response.Response;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
@@ -18,6 +20,12 @@ import static org.hamcrest.Matchers.*;
 class client {
     static void call(String url, String attribute, String expectation) {
         given().when().get(url).then().statusCode(200).body(attribute, equalTo(expectation));
+    }
+
+    static void call(String url, Map<String, String> expectation) {
+        Response response = given().when().get(url);
+        expectation.keySet().stream().forEach(
+                attribute -> response.then().body(attribute, equalTo(expectation.get(attribute))));
     }
 
     static void call(String url, String attribute, List<String> expectation) {
@@ -46,20 +54,20 @@ public class StepDefinitions {
         log = Logger.getLogger("Exchange rate test logger");
     }
 
-    @Given("Exchange Rate API {string}")
+    @Given("exchange rate API is {string}")
     public void exchange_rate(String url) {
         log.info("Initialize url: " + url);
         this.baseurl = url;
     }
 	
-	@Given("Exchange Rate API {string} and Base {string}")
+	@Given("exchange rate API is {string} and Base {string}")
 	public void exchange_rate_with_base(String url, String base){
         log.info("Initialize url: " + url + " base: " + base);
         this.baseurl = url;
         this.base = base;
 	}
 	
-	@Given("Exchange Rate API {string}, Symbols {string} and Base {string}")
+	@Given("exchange rate API is {string}, Symbols {string} and Base {string}")
 	public void exchange_rate_for_symbols_with_base(String url, String base, String symbols){
         log.info("Initialize url: " + url + " base: " + base + " symbols: " + symbols);
         this.baseurl = url;
@@ -67,20 +75,20 @@ public class StepDefinitions {
         this.symbols = symbols;
 	}
 	
-	@Given("Exchange Rate API {string} and Symbols {string}")
+	@Given("exchange rate API is {string} and Symbols {string}")
 	public void exchange_rate_for_symbols(String url, String symbols){
         log.info("Initialize url: " + url + " symbols: " + symbols);
         this.baseurl = url;
         this.symbols = symbols;
 	}
 	
-	@Given("Date is {string}")
+	@Given("date is {string}")
 	public void date_is(String date){
         log.info("Initialize date: " + date);
         this.date = date;
 	}
 
-    @When("Make a query of exchange rate")
+    @When("make a query of exchange rate")
     public void make_query_for_exchange() {
         log.info("Creating base url");
         if(!this.date.equals(""))
@@ -96,24 +104,29 @@ public class StepDefinitions {
         log.info("Created exchange rate api: " + this.baseurl);
     }
 
-    @Then("Response ok")
+    @Then("response ok")
     public void response_ok() {
         client.call(baseurl);
     }
 
-    @Then("Response have base {string}")
+    @Then("response have base {string}")
     public void has_response_base(String base){
         client.call(this.baseurl, "base", base);
     }
 
-    @Then("Response have symbols {string}")
+    @Then("response have symbols {string}")
     public void has_response_symbols(String symbols){
 	    List<String> _symbol = Arrays.asList(symbols.split(","));
         client.call(this.baseurl, "rates", _symbol);
     }
 
-    @Then("Response must have invalid")
+    @Then("response must have invalid")
     public void has_invalid_response(){
 	    client.invalid_call(this.baseurl);
+    }
+
+    @Then("response have base {string} and date {string}")
+    public void has_response_symbols_with_date(String base, String date){
+        client.call(this.baseurl, Map.of("base", base, "date", date));
     }
 }
--- src/test/resources/hellocucumber/exchange_rates.feature
+++ src/test/resources/hellocucumber/exchange_rates.feature
@@ -2,27 +2,27 @@ Feature: Exchange rate
   We will have exchange rate between currencies
 
   Scenario: Query exchange rate and response ok
-    Given Exchange Rate API "https://api.ratesapi.io/api"
-    When Make a query of exchange rate
-    Then Response ok
+    Given exchange rate API is "https://api.ratesapi.io/api"
+    When make a query of exchange rate
+    Then response ok
     
   Scenario: Make queries of exchange with base USD
-    Given Exchange Rate API "https://api.ratesapi.io/api" and Base "USD"
-    When Make a query of exchange rate
-    Then Response have base "USD"
+    Given exchange rate API is "https://api.ratesapi.io/api" and Base "USD"
+    When make a query of exchange rate
+    Then response have base "USD"
 
   Scenario: Make queries of exchange for symbols
-    Given Exchange Rate API "https://api.ratesapi.io/api" and Symbols "JPY,TRY"
-    When Make a query of exchange rate
-    Then Response have symbols "JPY,TRY"
+    Given exchange rate API is "https://api.ratesapi.io/api" and Symbols "JPY,TRY"
+    When make a query of exchange rate
+    Then response have symbols "JPY,TRY"
 
   Scenario: Make queries of exchange with incorrect information
-    Given Exchange Rate API "https://api.ratesapi.io/api" and Symbols "JPY,TY"
-    When Make a query of exchange rate
-    Then Response must have invalid
+    Given exchange rate API is "https://api.ratesapi.io/api" and Symbols "JPY,TY"
+    When make a query of exchange rate
+    Then response must have invalid
 
   Scenario: Make queries of exchange in specific date
-    Given Exchange Rate API "https://api.ratesapi.io/api" and Symbols "JPY,TRY"
-    Given Date is "2021-01-26"
-    When Make a query of exchange rate
-    Then Response have symbols "JPY,TRY"
+    Given exchange rate API is "https://api.ratesapi.io/api" and Symbols "JPY,TRY"
+    Given date is "2021-01-26"
+    When make a query of exchange rate
+    Then response have base "EUR" and date "2021-01-26"
